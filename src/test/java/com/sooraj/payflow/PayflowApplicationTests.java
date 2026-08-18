9a4af384f348428c3ebbdf1c07754ae16b411bac
sooraj2145 <soorajcnair@gmail.com>
2026-08-18 17:29:04 +0530

    Add TestContainers for isolated integration testing

--- src/test/java/com/sooraj/payflow/PayflowApplicationTests.java
+++ src/test/java/com/sooraj/payflow/PayflowApplicationTests.java
@@ -2,12 +2,26 @@ package com.sooraj.payflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class PayflowApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Configuration
+	static class TestContainersConfig {
+
+		@Bean
+		@ServiceConnection
+		PostgreSQLContainer<?> postgresContainer() {
+			return new PostgreSQLContainer<>("postgres:17");
+		}
 	}
 
 }
